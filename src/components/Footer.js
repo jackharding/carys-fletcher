e545dded562fa1c59572db4339c305e8cc82cd33
Jack Harding <jack@biglemon.co.uk>
2020-01-21 18:45:36 +0000

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer``;
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+
+    </StyledFooter>
+  );
+}
+
+export default Footer;
